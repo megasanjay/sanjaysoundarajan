0c994ba386de2e91f410efa5b8a05dfd21e3d848
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2021-08-01 20:22:25 -0700

    Added construction page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,68 @@
 <template>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <the-header></the-header>
+  <div class="relative">
+    <div class="flex flex-col justify-center items-center h-screen">
+      <h1
+        class=" text-xl sm:text-4xl md:text-5xl lg:text-6xl font-black font-mono relative construction"
+      >
+        Page under construction...
+      </h1>
+    </div>
+    <div class="absolute bottom-2 right-2 text-gray-200">
+      <div class="inline-block sm:hidden">mb</div>
+      <div class="hidden sm:inline-block md:hidden">sm</div>
+      <div class="hidden md:inline-block lg:hidden">md</div>
+      <div class="hidden lg:inline-block">lg</div>
+    </div>
+  </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
- import TheHeader from './components/UI/TheHeader.vue'
+import TheHeader from "./components/UI/TheHeader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    TheHeader
-  }
-}
+    TheHeader,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+h1::before,
+h1::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: -5px;
+  left: 0;
+}
+
+h1::before {
+  background: #fff;
+  animation: typewriter 4s steps(26) 1s forwards;
+}
+
+h1::after {
+  width: 0.125em;
+  background: black;
+  animation: typewriter 4s steps(26) 1s forwards, blink 750ms steps(26) infinite;
+}
+
+@keyframes typewriter {
+  to {
+    left: 100%;
+  }
+}
+
+@keyframes blink {
+  to {
+    background: transparent;
+  }
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        asap: ["Asap"],
+      },
+    },
   },
   variants: {
     extend: {},

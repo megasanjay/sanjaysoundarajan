94dea8c27a7e6745db2e87b4bb48d6c31035f4ca
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2021-08-11 01:28:05 -0700

    Split up components

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,8 @@
 <template>
   <Particles id="tsparticles" :options="particlesOptions" />
   <the-header></the-header>
-  <div class="relative">
-    <div class="flex flex-col justify-center items-center h-screen">
-      <div
-        class="
-          text-2xl
-          sm:text-4xl
-          md:text-5xl
-          lg:text-6xl
-          font-black
-          text-white
-          font-mono
-          text-center
-        "
-      >
-        <div
-          class="relative"
-          :class="{ 'typewriter-text': displayList.sentence1.showCursor }"
-        >
-          {{ this.displayList.sentence1.displayedSentence }}
-        </div>
-        <div
-          class="relative"
-          :class="{ 'typewriter-text': displayList.sentence2.showCursor }"
-        >
-          {{ this.displayList.sentence2.displayedSentence }}
-        </div>
-        <div
-          class="relative"
-          :class="{ 'typewriter-text': displayList.sentence3.showCursor }"
-        >
-          {{ this.displayList.sentence3.displayedSentence }}
-        </div>
-      </div>
-    </div>
 
-    <div class="absolute bottom-2 right-2 text-gray-200">
-      <div class="inline-block sm:hidden">mb</div>
-      <div class="hidden sm:inline-block md:hidden">sm</div>
-      <div class="hidden md:inline-block lg:hidden">md</div>
-      <div class="hidden lg:inline-block">lg</div>
-    </div>
-  </div>
+  <router-view> </router-view>
 </template>
 
 <script>
@@ -55,30 +15,6 @@ export default {
   },
   data() {
     return {
-      displayList: {
-        sentence1: {
-          sentence: "Hi!",
-          displayedSentence: "",
-          showCursor: false,
-          counter: 0,
-          steps: 3,
-        },
-        sentence2: {
-          sentence: "I'm Sanjay Soundarajan :)",
-          displayedSentence: "",
-          showCursor: false,
-          counter: 0,
-          steps: 11,
-        },
-        sentence3: {
-          sentence: "Nice to meet  you :)",
-          displayedSentence: "",
-          showCursor: false,
-          counter: 0,
-          steps: 20,
-        },
-      },
-
       particlesOptions: {
         background: { color: { value: "#1F2937" } },
         fullScreen: { enable: true, zIndex: -1 },
@@ -113,7 +49,6 @@ export default {
               factor: 10,
               speed: 10,
             },
-
             bounce: {
               horizontal: {
                 random: {
@@ -191,70 +126,11 @@ export default {
       },
     };
   },
-  methods: {
-    typingFunction: function (key, interval, keepCursor = false) {
-      let that = this;
-      const keyString = key;
-      const typingFunction = setInterval(function () {
-        that.displayList[keyString].showCursor = true;
-        if (
-          that.displayList[keyString].counter >=
-          that.displayList[keyString].sentence.length
-        ) {
-          that.displayList[keyString].displayedSentence =
-            that.displayList[keyString].sentence;
-          that.displayList[keyString].counter =
-            that.displayList[keyString].sentence.length;
-          clearInterval(typingFunction);
-          if (!keepCursor) {
-            that.displayList[keyString].showCursor = false;
-          }
-        } else {
-          1;
-          that.displayList[keyString].displayedSentence = that.displayList[
-            keyString
-          ].sentence.substring(0, that.displayList[keyString].counter);
-          that.displayList[keyString].counter++;
-        }
-      }, interval / that.displayList[keyString].sentence.length);
-    },
-  },
-  mounted() {
-    let that = this;
-    setTimeout(() => that.typingFunction("sentence1", 1000), 100);
-    setTimeout(() => that.typingFunction("sentence2", 6000, true), 1500);
-    // setTimeout(() => that.typingFunction("sentence3", 4000, true), 4800);
-  },
 };
 </script>
 
 <style>
 #app {
   color: #2c3e50;
-}
-
-.typewriter-text {
-  width: max-content;
-}
-
-.typewriter-text::after {
-  content: "";
-  position: absolute;
-  top: -5px;
-  right: 0;
-  bottom: -5px;
-  left: 100%;
-  width: 0.125em;
-  background: white;
-}
-
-.typewriter-text::after {
-  animation: blink 700ms infinite;
-}
-
-@keyframes blink {
-  to {
-    background: transparent;
-  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from "vue";
+import { router } from "./router";
 import App from "./App.vue";
 import "./index.css";
+
 import Particles from "particles.vue3";
+
 const app = createApp(App);
-// app.use(router);
+
+app.use(router);
 app.use(Particles);
+
 app.mount("#app");

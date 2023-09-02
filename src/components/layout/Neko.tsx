3e5b31c39488e967bcb86bbfe2d8ef4c017640c0
Sanjay Soundarajan <sanjay.16@live.com>
2023-09-02 15:11:33 -0700

    feat: ✨ add a cat via neko-ts [ABSanthosh/neko-ts] (#520)

---
+++ src/components/layout/Neko.tsx
@@ -0,0 +1,41 @@
+import { Neko as NekoClass } from 'neko-ts';
+import { useEffect, useRef, useState } from 'react';
+
+export default function Neko() {
+  const neko = useRef<NekoClass>();
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const toggleNeko = () => {
+    if (isPlaying) {
+      neko.current?.sleep();
+    } else {
+      neko.current?.wake();
+    }
+
+    setIsPlaying(!isPlaying);
+  };
+
+  useEffect(() => {
+    if (!neko.current) {
+      neko.current = new NekoClass({
+        origin: {
+          x: 100,
+          y: window.innerHeight - 90,
+        },
+      });
+    }
+  }, [neko]);
+
+  return (
+    <div className="relative neko-container">
+      <div className="fixed bottom-5 left-5 neko-box">
+        <button
+          className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none"
+          onClick={toggleNeko}
+        >
+          {isPlaying ? 'Get some rest, Neko' : "Let's play, Neko!"}
+        </button>
+      </div>
+    </div>
+  );
+}
